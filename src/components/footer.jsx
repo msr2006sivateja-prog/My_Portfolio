c55e785be4db568edfa2407e77c83c6c65ae7b3d
siva teja <msr2006sivateja@gmail.com>
2026-08-21 13:10:50 +0530

    Update portfolio components and links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,15 +5,19 @@ function Footer() {
             <p>© 2026 Siva Teja. All rights reserved.</p>
 
             <div className="footer-links">
-                <a href="mailto:your-email@example.com">
+                <a
+                    href="https://mail.google.com/mail/?view=cm&fs=1&to=sm24csb0a73@student.nitw.ac.in"
+                    target="_blank"
+                    rel="noreferrer"
+                >
                     Email
                 </a>
 
-                <a href="#" target="_blank">
+                <a href="https://github.com/msr2006sivateja-prog" target="_blank">
                     GitHub
                 </a>
 
-                <a href="#" target="_blank">
+                <a href="https://www.linkedin.com/in/meda-sivateja-771b8932b/" target="_blank">
                     LinkedIn
                 </a>
             </div>
